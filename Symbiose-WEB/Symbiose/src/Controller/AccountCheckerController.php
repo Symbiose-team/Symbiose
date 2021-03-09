69575228721ed6318a0ed3624c2b52ab9bbb949b
SkanderThabet <78706653+SkanderThabet@users.noreply.github.com>
2021-03-09 01:48:09 +0100

    Confirmation par mail effectuée, ce qui me reste userchecker, session , token et update BD

---
+++ Symbiose-WEB/Symbiose/src/Controller/AccountCheckerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User as AppUser;
+use App\Exception\AccountDeletedException;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AccountCheckerController implements UserCheckerInterface
+{
+
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+
+        // user is deleted, show a generic Account Not Found message.
+        if ($user->isDeleted()) {
+            throw new AccountDeletedException();
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+
+        // user account is expired, the user may be notified
+        if ($user->isExpired()) {
+            throw new AccountExpiredException('...');
+        }
+    }
+}
---
+++ Symbiose-WEB/Symbiose/src/Repository/TestRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Test;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @method Test|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Test|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Test[]    findAll()
+ * @method Test[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TestRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Test::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    {
+        if (!$user instanceof Test) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newEncodedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    // /**
+    //  * @return Test[] Returns an array of Test objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Test
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
