c8f4cec890d1cfcc64597799b6a378e6fb99c641
oussema-mestiri2 <78915618+oussema-mestiri2@users.noreply.github.com>
2021-03-11 13:10:59 +0100

    Part 4.5.2
    other fixes

---
+++ Symbiose-WEB/Symbiose/src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="homepage")
+     */
+    public function home()
+    {
+        $prenom=["Oussema"=>23,"Ramy"=>10,"Anne"=>24];
+        {
+            return $this->render('home/index.html.twig', [
+                'title'=>'Aurevoir tout le monde',
+                'age'=>12,
+                'tableau'=>$prenom
+            ]);
+        }
+    }
+}
--- Symbiose-WEB/Symbiose/src/Form/ProductType.php
+++ Symbiose-WEB/Symbiose/src/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Product;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,9 @@ class ProductType extends AbstractType
             ->add('Name')
             ->add('Description', TextareaType::class)
             ->add('Price')
-            ->add('Type')
+            ->add('Type', ChoiceType::class, [
+                'choices' => $this->getChoices()
+            ])
             ->add('State', null, [
                 'label' => 'Sold'
             ])
@@ -30,5 +33,15 @@ class ProductType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Product::class,
         ]);
+    }
+
+    private function getChoices()
+    {
+        $choices = Product::ARTICLE;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
     }
 }
