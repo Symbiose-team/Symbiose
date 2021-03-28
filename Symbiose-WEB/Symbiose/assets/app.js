a65c3c451f6819a7912855c85fb624b4c58f95ef
oussema-mestiri2 <78915618+oussema-mestiri2@users.noreply.github.com>
2021-03-28 23:04:58 +0100

    Part 4.8.3
    working on cart interface

--- Symbiose-WEB/Symbiose/assets/app.js
+++ Symbiose-WEB/Symbiose/assets/app.js
@@ -10,14 +10,3 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-
-// var slider = document.getElementById('slider');
-//
-// noUiSlider.create(slider, {
-//     start: [20, 80],
-//     connect: true,
-//     range: {
-//         'min': 0,
-//         'max': 100
-//     }
-// })
