bc36425ab35a06283ce6e3beed8795e8b8e500b5
SkanderThabet <78706653+SkanderThabet@users.noreply.github.com>
2021-03-30 21:55:45 +0200

    Fixing composer install

--- Symbiose-WEB/Symbiose/config/bundles.php
+++ Symbiose-WEB/Symbiose/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
